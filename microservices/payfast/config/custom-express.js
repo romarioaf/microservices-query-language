822f059f4cd77c74bfa4eed0d8404ae9203f0afa
Romário Freitas <romarioaf1989@gmail.com>
2017-03-13 00:04:27 -0300

    Inclusão do middleware extractor no projeto payfast.

--- microservices/payfast/config/custom-express.js
+++ microservices/payfast/config/custom-express.js
@@ -2,13 +2,14 @@ var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
+var extractor = require('../../../extractor/extractor');
 
 module.exports = function() {
 	var app = express();
-
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.use(expressValidator());
+	app.use(extractor());
 
 	consign()
 		.include('controllers')
--- microservices/payfast/index.js
+++ microservices/payfast/index.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')();
 
-app.listen('3000', function () {
-	console.log('Servidor rodando...');
+app.listen('3003', function () {
+	console.log('Servidor Payfast em execução.');
 });
